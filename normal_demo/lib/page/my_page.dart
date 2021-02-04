f5ebc90305b579dcab9ef06f74746ecc3c6d9c4c
季风 <jihq@servyou.com.cn>
2021-02-04 09:19:29 +0800

    first commit

---
+++ normal_demo/lib/page/my_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:normal_demo/model/account_model.dart';
+import 'package:normal_demo/page/login_page.dart';
+import 'package:normal_demo/page/modify_userinfo_page.dart';
+import 'package:normal_demo/utils/account_manager.dart';
+import 'package:normal_demo/utils/event_bus_util.dart';
+
+class MyPage extends StatelessWidget {
+  final String title;
+  const MyPage({Key key,this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body: _MyPageBody(),
+      ),
+    );
+  }
+}
+
+class _MyPageBody extends StatefulWidget {
+  _MyPageBody({Key key}) : super(key: key);
+
+  @override
+  _MyPageBodyState createState() => _MyPageBodyState();
+}
+
+class _MyPageBodyState extends State<_MyPageBody> {
+  bool showLogoutButton;
+  String userName;
+  String name;
+  void refreshState() {
+    showLogoutButton = !(AccountManager.instance.accountModel.accountID == null ||
+        AccountManager.instance.accountModel.accountID.length == 0);
+    userName = AccountManager.instance.accountModel?.userName;
+    name = AccountManager.instance.accountModel?.userModel?.name;
+  }
+
+  @override
+  void initState() {
+    refreshState();
+    EventBusUtil.instance.on<AccountModelUpdatedEvent>().listen((onData) {
+      setState(() {
+        refreshState();
+      });
+    });
+    super.initState();
+  }
+
+  void unloginButtonPressed() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return LoginPage();
+    }));
+  }
+
+  void modifyNameButtonPressed() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return ModifyUserInfoPage();
+    }));
+  }
+  
+  void logoutButtonPressed() {
+    AccountModel accountModel = AccountModel();
+    AccountManager.instance.saveInfo(accountModel);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        Visibility(
+          visible: !showLogoutButton,
+          child: FlatButton(
+            onPressed: unloginButtonPressed, 
+            child: Text('用户未登录')
+          ),
+        ),
+        Visibility(
+          visible: showLogoutButton,
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.account_circle, size: 44,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(userName??''),
+                  Text(name??''),
+                ],
+              ),
+            ],
+          )
+        ),
+        Visibility(
+          visible: showLogoutButton,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: modifyNameButtonPressed,
+                  child: Text(
+                    '修改姓名',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.blue[100],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Visibility(
+          visible: showLogoutButton,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: logoutButtonPressed,
+                  child: Text(
+                    '退出登录',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ normal_demo/lib/page/root_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:normal_demo/page/home_page.dart';
+import 'package:normal_demo/page/my_page.dart';
+
+class RootPage extends StatefulWidget {
+
+  String tabName;
+
+  static const String HomeTab = "HomeTab";
+  static const String MyTab = "MyTab";
+
+  RootPage({
+    String tabName
+  }) : this.tabName = tabName;
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> with SingleTickerProviderStateMixin {
+
+  TabController _controller;
+  var _index = 0;
+
+  @override
+  void initState(){
+      super.initState();
+      _controller = new TabController(length: 2, vsync: this);
+      _controller.addListener((){
+        setState(() {
+          _index = _controller.index;
+        });
+      });
+
+      if(widget.tabName != null && widget.tabName.isNotEmpty) {
+        switch(widget.tabName) {
+          case RootPage.HomeTab:
+            _controller.index = 0;
+            break;
+          case RootPage.MyTab:
+            _controller.index = 1;
+            break;
+        }
+      }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: TabBarView(
+          controller: _controller,
+          children: <Widget>[
+            HomePage(),
+            MyPage(title: '我的',)
+          ],
+        ),
+        bottomNavigationBar: Container(
+          height: 60,
+          child: TabBar(
+            labelStyle: TextStyle(
+              fontSize: 11,
+            ),
+            controller: _controller,
+            tabs: <Widget>[
+              Tab(
+                text: "首页",
+                icon: _index == 0 ? Icon(Icons.home, color: Color(0xffffba0a),) : Icon(Icons.home)
+              ),
+              Tab(
+                text: "我的",
+                icon: _index == 1 ? Icon(Icons.account_box, color: Color(0xffffba0a),) : Icon(Icons.account_box)
+              )
+            ],
+          ),
+        ),
+          
+        );
+  }
+}
