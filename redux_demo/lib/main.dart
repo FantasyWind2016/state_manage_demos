0227f96e7797a52d8e52fba1db49301b40f70455
季风 <jihq@servyou.com.cn>
2021-03-01 14:47:32 +0800

    + 普通模式代码初始化

--- redux_demo/lib/main.dart
+++ redux_demo/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'page/root_page.dart';
+import 'utils/account_manager.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    AccountManager.instance.loadInfo();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -20,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: RootPage(),
     );
   }
 }
---
+++ redux_demo/lib/page/login_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import '../model/account_model.dart';
+import '../model/user_model.dart';
+import '../utils/account_manager.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: _LoginPageBody(),
+      ),
+    );
+  }
+}
+
+class _LoginPageBody extends StatefulWidget {
+  _LoginPageBody({Key key}) : super(key: key);
+
+  @override
+  _LoginPageBodyState createState() => _LoginPageBodyState();
+}
+
+class _LoginPageBodyState extends State<_LoginPageBody> {
+  void loginButtonPressed() {
+    AccountModel accountModel = AccountModel();
+    accountModel.accountID = 'u123456';
+    accountModel.userName = userNameController.text;
+    accountModel.password = passwordController.text;
+    AccountManager.instance.saveInfo(accountModel);
+
+    Navigator.of(context).pop();
+  }
+
+  var userNameController = TextEditingController();
+  var passwordController = TextEditingController();
+  bool loginButtonEnable = false;
+  void onTextChanged(_){  
+    setState(() {
+      loginButtonEnable = (userNameController.text != null && userNameController.text.length>0) && (passwordController.text != null && passwordController.text.length>4);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          children: <Widget>[
+            Text('用户名：'),
+            Expanded(
+              child: TextField(
+                controller: userNameController,
+                onChanged: onTextChanged,
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text('密码：'),
+            Expanded(
+              child: TextField(
+                controller: passwordController,
+                onChanged: onTextChanged,
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: loginButtonEnable ? loginButtonPressed : null,
+                child: Text(
+                  '登录',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Colors.green[600],
+                disabledColor: Colors.grey[400],
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
